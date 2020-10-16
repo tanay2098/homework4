dafcef775dbd8c8e9625e8452a74b91737feca75
Tanay <tanay.patel@student.csulb.edu>
2020-10-16 15:54:21 +0530

    commit3

--- task2.py
+++ task2.py
@@ -6,12 +6,12 @@ for i in range(0,2):
 t1 = tuple(nums)
     
 correct_answer = (int)(t1[0] * t1[1] )
-print("How much is",t1[0],"times",t1[1])
+print("How much is",t1[0],"times",t1[1],"? ")
 temp = 0 
 while(temp<1):
-    user_answer = int(input(""))
+    user_answer = int(input("->"))
     if user_answer==correct_answer:
         print("done")
         temp= temp +1
     else:
-        print(t1[0],"times",t1[1],"is not",user_answer,"please try again:")
+        print(t1[0],"times",t1[1],"is not,",user_answer,"please try again: ")
