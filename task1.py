e46424e55d74557684305bd7de73d926c955ba54
Tanay <tanay.patel@student.csulb.edu>
2020-10-16 17:52:58 +0530

    commit5

--- task1.py
+++ task1.py
@@ -1,19 +1,19 @@
-import random
+import random #importing random 
 
-def is_prime(n):
+def is_prime(n):# defining is_prime function 
     if n>1:
         for i in range(2,n):
-            if((n%i)==0):
-                return False
+            if((n%i)==0): # Checking divisibility of the number
+                return False # returns False boolean value if number is not prime
         else:
-            return True
+            return True # returns True boolean value if number is prime
     else:
-        return False                    
+        return False  # # returns False boolean value if number is not prime                  
 
-for i in range(0,6):
-    n=random.randrange(1,101)
-    ans=is_prime(n)
-    if ans==True:
+for i in range(0,6): # Loop for 6 numbers
+    n=random.randrange(1,101) # generating 6 random numbers between 1 and 101, so 1 and 100 are included. 
+    ans=is_prime(n)# applying n in the is_prime function and assigning its value to ans.
+    if ans==True: # if is_prime returns true 
         print('The random number',n,'is a prime number')
-    else:
+    else:# if is_prime returns false
         print('The random number',n,'is not a prime number')
